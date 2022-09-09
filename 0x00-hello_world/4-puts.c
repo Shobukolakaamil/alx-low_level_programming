378981b2e62b93196731661c26876c85b5983de3
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-08 20:59:08 -0700

    Printing a string usint puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
  main - Print the string using puts function
  *
  *
- Description : Using the main function 
+ Description: Using the main function 
  *
- This program prints "Programming is like building a multilingual puzzle" 
- * Return : 0
+ This program prints "Programming is like building a multilingual puzzle 
+ * Return: 0
  */
- 
-int main(void)
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
