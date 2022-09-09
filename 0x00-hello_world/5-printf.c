3de8cdc5fe7257517b8a8aa2d6923a8ff86aed41
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-08 21:06:29 -0700

    Printing a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>                                                                                                                                     
+
+/**                                                                                                                                                    
+ *
+ *                                                                                                                                                      
+ *
+ *   main - Print the string using puts function                                                                                                           
+ *
+ *    *                                                                                                                                                     
+ *
+ *     *                                                                                                                                                     
+ *
+ *      Description: Using the main function                                                                                                                  
+ *
+ *       *                                                                                                                                                     
+ *
+ *        This program prints "Programming is like building a multilingual puzzle                                                                               
+ *
+ *         * Return: 0                                                                                                                                           
+ *
+ *          */                                                                                                                                                    
+
+ int main(void)                                                                                                                                        
+
+{                                                                                                                                                      
+
+	        pintf("\"with proper grammar, but the outcome is a piece of art,\n");                                                                                  
+
+		return (0);
+}
