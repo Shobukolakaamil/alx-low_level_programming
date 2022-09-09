188d6367526ef40cf1fe332eb8c6879633ba7877
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-08 22:15:08 -0700

    Printing using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,30 +1,20 @@
 #include <stdio.h>                                                                                                                                     
 
 /**                                                                                                                                                    
- *
- *                                                                                                                                                      
- *
- *   main - Print the string using puts function                                                                                                           
- *
- *    *                                                                                                                                                     
- *
- *     *                                                                                                                                                     
- *
- *      Description: Using the main function                                                                                                                  
- *
- *       *                                                                                                                                                     
- *
- *        This program prints "Programming is like building a multilingual puzzle                                                                               
- *
- *         * Return: 0                                                                                                                                           
- *
- *          */                                                                                                                                                    
+*
+*main - Print the string using puts function                                                                                                           
+*
+*  
+*Description: Using the main function                                                                                                                  
+*                                                                                                                                                    
+*this program prints "Programming is like building a multilingual puzzle                                                                               
+*
+* Return: 0                                                                                                                                           
+*/                                                                                                                                                    
 
- int main(void)                                                                                                                                        
+int main(void)                                                                                                                                        
 
-{                                                                                                                                                      
-
-	        pintf("\"with proper grammar, but the outcome is a piece of art,\n");                                                                                  
-
-		return (0);
+{                                                                                                                                                   
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");                                                                  
+	return (0);
 }
