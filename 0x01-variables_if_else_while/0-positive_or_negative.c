a0edc81a6e80a4d66c7dcadef74a8a37f06fd99b
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-09 20:50:47 -0700

    Printing random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,6 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
-
-
 int main(void)
 
 {
@@ -22,15 +20,15 @@ int main(void)
 
 		n = rand() - RAND_MAX / 2;
 
-		if (n>0)
+		if (n > 0)
 		{
 			printf("%i is positive\n", n);
 		}
-		else if (n==0)
+		else if (n == 0)
 		{
 			printf("%i is zero\n", n);
 		}
-		else if (n<0)
+		else if (n < 0)
 		{
 			printf("%i is negative\n", n);
 		}
