8d3cf6e1abd0bc60747b4287e50c5ecc3d511b78
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-09 20:22:14 -0700

    Printing random numbers

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -2,7 +2,13 @@
 
 #include <time.h>
 
-/* more headers goes there */
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+*/
 
 
 
@@ -18,7 +24,18 @@ int main(void)
 
 		n = rand() - RAND_MAX / 2;
 
-		/* your code goes there */
+		if (n>0)
+		{
+			printf("%i is positive\n", n);
+		}
+		else if (n==0)
+		{
+			printf("%i is zero\n", n);
+		}
+		else if (n<0)
+		{
+			printf("%i is negative\n", n);
+		}
 
 		return (0);
 
