9bb4eea2cefe92702dd20b62fa942a1743e7e8bd
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-11 14:16:37 -0700

    Printing numbers between 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print if the number is positive, zero, or negative
+ *
+ *Description: using the main function
+ *this program print "Programming is positive, zero, or negative
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%n", n);
+	}
+
+	printf("\n");
+	return (0);
+
+}
