bbfdfa4fa03253cc807732ae8d9bd316fa208013
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-10 23:02:46 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return: 0 
+ * Return:0 
  */
 int main(void)
 
@@ -18,12 +18,12 @@ int main(void)
 
 
 		srand(time(0));
-	
+
 		n = rand() - RAND_MAX / 2;
-	
+
 		l = n % 10;
-		
-		
+
+
 		if (l > 5)
 		{
 			printf("Last digit of %d is %d and is greater than 5/n", n, l);
@@ -36,7 +36,7 @@ int main(void)
 		{
 			printf("Last digit of %d is %d and is less than 6 and not 0/n", n, l);		
 		}
-	
+
 		return (0);
 
 }
