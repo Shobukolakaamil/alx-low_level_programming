b965351f75f4c1e8c56034fe0f19f29ffa75ff1f
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-11 13:52:02 -0700

    Printing low case and upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,13 @@
  *
  * Deccription: using the main function
  *
- * this program prints "Programming is positive, zero, or negative 
+ * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
