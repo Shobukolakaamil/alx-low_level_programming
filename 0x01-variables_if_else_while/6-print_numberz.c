6f501ec24cff20a2ef83f881b3982884935c0edd
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-11 21:09:48 -0700

    Printing single digit numbers in base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-	int n;
+		int n;
 
-	for (n = '0'; n<= '9'; n++)
-	{
-		putchar(n)
-	}
+		for (n = '0'; n<= '9'; n++)
+		{
+			putchar(n);
+		}
 
-	putchar('\n')
+		putchar('\n');
 	
-		return(0)
+		return(0);
 
 }
