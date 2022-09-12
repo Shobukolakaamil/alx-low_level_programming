5d14375cfddb25c5187d6c22afe5b4aa985e3669
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-11 23:07:32 -0700

    Printing numbers seperated by comma ans whitespace

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all possible combination of single digit numbers
+ *
+ * Description: using the main function
+ * this program prints "Single digit numbers
+ * Return: 0
+ */
+int main(void)
+
+{
+	int h;
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		putchar (h);
+	}
+
+	putchar (',');
+	putchar (' ');
+
+}
