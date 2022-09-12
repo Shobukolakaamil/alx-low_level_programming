9f3eae3a26412893441a9579f6cafceb7191b5d2
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-11 22:21:06 -0700

    Printing lowercase alphabets in reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print if the number is positive, zero or negative 
+ * main - print if the number is positive, zero or negative
  *
  * Description: using main function
  * this program prints "Lower case alphabets in reverse
