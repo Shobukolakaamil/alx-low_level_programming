87194872337622b9722c49776b636691050f000f
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-13 23:14:32 -0700

    Printing Jack Bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins
+ * starting from 00:00 to 23:59, min loop counts mins, while hour loop counts
+ * hours and resets mins
  * Return: 0
  */
 void jack_bauer(void)
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remainder;
-       	int mins_remainder;
+	int mins_remainder;
 	while (hours <= 23)
 	{
 		while (minutes <= 59)
