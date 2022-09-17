05a76e97d0bb1099130ac39797a0c2b3dd5d9ee3
Shobukolakaamil <kaamil0044real1244@gmail.com>
2022-09-16 22:03:35 -0700

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
